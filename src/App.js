74a024b12ed6feeaefed7ef603e7eb2b8fbde217
bexalho <aljesa.hoxha@bexeo.com>
2022-10-04 15:58:34 +0200

    adding a user list component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import AddUser from "./components/Users/AddUser";
 import "./App.css";
+import UsersList from "./components/Users/UsersList";
 
 function App() {
   return (
     <div>
       <AddUser />
+      <UsersList users={[]} />
     </div>
   );
 }
