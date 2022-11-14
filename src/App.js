20b493846cc16ccc08791c0a28e5804c9455019f
bexalho <aljesa.hoxha@bexeo.com>
2022-11-14 21:08:30 +0000

    added error modal and add user functions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { useState } from "react";
 import AddUser from "./components/Users/AddUser";
 import "./App.css";
 import UsersList from "./components/Users/UsersList";
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const AddUserHandler = (userName, userAge) => {
+    setUsersList((prevUsersList) => {
+      return [...prevUsersList, {name: userName, age: userAge, id: Math.random().toString()}];
+    });
+  };
+
   return (
     <div>
-      <AddUser />
-      <UsersList users={[]} />
+      <AddUser onAddUser={AddUserHandler}/>
+      <UsersList users={usersList} />
     </div>
   );
 }
